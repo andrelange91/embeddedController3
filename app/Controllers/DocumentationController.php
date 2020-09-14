7a09f9be1ab13610d0ca7f1f8281d4b2703e4fe5
André Lange <andre@lait.dk>
2020-09-14 13:00:42 +0200

    Added documentation View.

--- app/Controllers/DocumentationController.php
+++ app/Controllers/DocumentationController.php
@@ -4,12 +4,20 @@ namespace App\Controllers;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Twig\Environment as Twig;
 
 class DocumentationController
 {
+    private $twig;
+    public function __construct(Twig $twig){
+        $this->twig = $twig;
+    }
+
     public function DocumentationPage(Request $request, Response $response)
     {
-        $response->getBody()->write("Hello DocumentationPage");
+        $html = $this->twig->render('DocumentationPage.twig');
+
+        $response->getBody()->write($html);
         return $response;
     }
 }
