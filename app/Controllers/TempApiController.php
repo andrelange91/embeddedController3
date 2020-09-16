63f97b558a9493eb3755d25ff42d784e34be7e2e
André Lange <andre@lait.dk>
2020-09-16 10:21:33 +0200

    Added 3 api routes for temperature.

---
+++ app/Controllers/TempApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class TempApiController
+{
+
+    public function GetHighestTemperature(Request $request, Response $response)
+    {
+        $data = "31c";
+        $response->getBody()->write($data);
+        return $response;
+    }
+    public function GetLowestTemperature(Request $request, Response $response)
+    {
+        $data = "-1c";
+        $response->getBody()->write($data);
+        return $response;
+    }
+    public function GetMedianTemperature(Request $request, Response $response)
+    {
+        $data = "21c";
+        $response->getBody()->write($data);
+        return $response;
+    }
+}
--- routes.php
+++ routes.php
@@ -2,7 +2,13 @@
 // use for each controller.
 use App\Controllers\DocumentationController;
 use App\Controllers\HomeController;
+use App\Controllers\TempApiController;
 
 // define all routes for application
 $app->get('/', HomeController::class . ':FrontPage')->setName("frontPage");
 $app->get('/api/documentation', DocumentationController::class . ':DocumentationPage')->setName("documentation");
+
+// API Routes
+$app->get('/api/gethighesttemperature', TempApiController::class . ':GetHighestTemperature');
+$app->get('/api/getlowesttemperature', TempApiController::class . ':GetLowestTemperature');
+$app->get('/api/getmediantemperature', TempApiController::class . ':GetMedianTemperature');
