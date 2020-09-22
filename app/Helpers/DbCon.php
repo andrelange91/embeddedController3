da3b8dec1c65a92080b9848952d9e72b1bad5b47
André Lange <andre@lait.dk>
2020-09-22 11:49:25 +0200

    asdasd

--- app/Helpers/DbCon.php
+++ app/Helpers/DbCon.php
@@ -3,6 +3,7 @@ namespace App\Helpers;
 
 class DbCon{
     private $connection = null;
+    private $allowed = ['', '00000'];
 
     public function getConnection() {
         if ($this->connection === null) {
@@ -24,7 +25,7 @@ class DbCon{
     public function runSetSql($sql)
     {
         $stmt = $this->getConnection()->query($sql);
-        if($stmt !== false && $stmt->errorCode() != '00000'){
+        if($stmt === false || !in_array($stmt->errorCode(), $this->allowed)){
             die("du har en fejl i din runSetSql. Tjek følgende:<br>".$sql);
         } else {
             return true;  
@@ -33,7 +34,7 @@ class DbCon{
     public function runGetSql($sql)
     {
         $stmt = $this->getConnection()->query($sql);
-        if($stmt !== false && $stmt->errorCode() != '00000'){
+        if($stmt === false || !in_array($stmt->errorCode(), $this->allowed)){
             die("du har en fejl i din runGetSql. Tjek følgende:<br>".$sql);
         } else {
             return $stmt->fetchAll();  
